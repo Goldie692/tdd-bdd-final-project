274244deadf7b9b2b61d7fef4cb5bd5791815461
Goldie692 <goldiwe97@gmail.com>
2026-03-08 22:15:28 -0400

    Final project complete - all tests passing

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -1,29 +1,23 @@
-######################################################################
-# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-######################################################################
-
-"""
-Product Steps
-
-Steps used for product.feature
-"""
-from behave import *
-from service.models import Product
+import requests
+from behave import given
 
 @given('the following products')
 def step_impl(context):
+    """ Delete all Products and load new ones """
+    rest_endpoint = f"{context.base_url}/products"
+    context.resp = requests.get(rest_endpoint)
+    assert(context.resp.status_code == 200)
+    for product in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
+        assert(context.resp.status_code == 204)
+        
     for row in context.table:
-        product = Product(name=row['name'], category=row['category'], price=float(row['price']), description=row['description'])
-        product.create()
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert(context.resp.status_code == 201)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -271,3 +271,16 @@ class TestProductRoutes(TestCase):
         # check the data just to be sure
         for product in data:
             self.assertEqual(product["available"], True)
+def test_delete_product(self):
+        """It should Delete a Product"""
+        products = self._create_products(5)
+        product_count = self.get_product_count()
+        test_product = products[0]
+        response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # make sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        new_count = self.get_product_count()
+        self.assertEqual(new_count, product_count - 1)
